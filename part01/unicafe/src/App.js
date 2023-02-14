ca644964ca54599a729c47b5f44555b9b6267938
Simo-Pekka Varis <simopekka.varis@gmail.com>
2023-02-15 00:58:22 +0200

    fix

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -27,24 +27,15 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
-  const [average, setAverage] = useState(0);
 
   const handleGoodClick = () => {
     setGood(good + 1)
-    setTotal(total + 1)
-    setAverage(1 / total)
-
   }
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
-    setTotal(total + 1)
-    setAverage(average / total)
   }
   const handleBadClick = () => {
     setBad(bad + 1)
-    setTotal(total + 1)
-    setAverage((average - 1) / total )
   }
 
 
@@ -55,7 +46,7 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text='neutral'/>
       <Button handleClick={handleBadClick} text='bad'/>
       <br/>
-      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   );
 }
