a6621366a1368d815392a695295b541f8b8303dc
Simo-Pekka Varis <simopekka.varis@gmail.com>
2023-02-17 16:50:01 +0200

    puhelinluettelo

---
+++ part02/puhelinluettelo/src/App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const Persons = ({person}) => (
+  <>
+    <p>{person.name}</p>
+  </>
+)
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+    console.log(nameObject)
+  }
+  const handleInputChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+          value={newName}
+          onChange={handleInputChange}
+        />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => 
+        <Persons key={person.name} person={person} />
+      )}
+    </div>
+  )
+
+}
+
+export default App
