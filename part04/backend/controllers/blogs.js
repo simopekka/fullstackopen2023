7bef45c2cf60f4412bd4fe01b5f6cca89e81cabd
Simo-Pekka Varis <simopekka.varis@gmail.com>
2023-09-13 17:39:48 +0300

    lint

--- part04/backend/controllers/blogs.js
+++ part04/backend/controllers/blogs.js
@@ -3,26 +3,26 @@ const Blog = require ('../models/blog')
 
 
 blogsRouter.get('/', (request, response) => {
-	Blog.find({}).then(blogs => {
-		response.json(blogs)
-	})
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
 })
   
 blogsRouter.post('/', (request, response) => {
-	const body = request.body
+  const body = request.body
     
-	const blog = new Blog({
-		title: body.title,
-		author: body.author,
-		url: body.url,
-		likes: body.likes
-	})
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
   
-	blog.save()
-		.then(result => {
-			response.status(201).json(result)
-		})
-		.catch(error => next(error))
+  blog.save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error))
 })
 
 module.exports = blogsRouter
--- part04/backend/utils/for_testing.js
+++ part04/backend/utils/for_testing.js
@@ -1,34 +1,34 @@
 const dummy = ( blogs ) => {
-	console.log(blogs)
-	return 1
+  console.log(blogs)
+  return 1
 }
 
 const totalLikes = ( blogs ) => {
-	if (blogs.length === 0) {
-		return 0
-	} else if (blogs.length === 1) {
-		return blogs[0].likes
-	} else {
-		var total = blogs.reduce((previous, current) => {
-			return previous + current.likes
-		}, 0)
-		return total
-	}
+  if (blogs.length === 0) {
+    return 0
+  } else if (blogs.length === 1) {
+    return blogs[0].likes
+  } else {
+    var total = blogs.reduce((previous, current) => {
+      return previous + current.likes
+    }, 0)
+    return total
+  }
 }
 
 const favoriteBlog = ( blogs ) => {
-	const mostLikes = blogs.reduce((previous, current) => {
-		if (current.likes >= previous) {
-			return current.likes
-		}
-		return previous
-	}, 0)
-	var blog = blogs.find(blog => blog.likes === mostLikes)
-	return blog
+  const mostLikes = blogs.reduce((previous, current) => {
+    if (current.likes >= previous) {
+      return current.likes
+    }
+    return previous
+  }, 0)
+  var blog = blogs.find(blog => blog.likes === mostLikes)
+  return blog
 }
 
 module.exports = {
-	dummy,
-	totalLikes,
-	favoriteBlog
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
